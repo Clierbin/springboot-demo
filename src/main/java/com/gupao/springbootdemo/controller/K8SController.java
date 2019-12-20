c2d9a58376a6aa65adc191fd48923b22216dfbc1
张斌 <q243132465@163.com>
2019-12-20 15:56:35 +0800

    reids host update

--- src/main/java/com/gupao/springbootdemo/controller/K8SController.java
+++ src/main/java/com/gupao/springbootdemo/controller/K8SController.java
@@ -2,6 +2,7 @@ package com.gupao.springbootdemo.controller;
 
 import com.gupao.springbootdemo.util.RedisUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ public class K8SController {
 
     @RequestMapping("/k8s")
     public String k8s(){
+        System.out.println("Hello K8s!!!");
         return "hello K8s Jack and zhangbin OK 666";
     }
     @Autowired
@@ -23,7 +25,7 @@ public class K8SController {
         if(!redisUtil.hasKey(key)){
             redisUtil.set(key, "bearPotMan");
         }
+        System.out.println("进入到了Redis");
         return redisUtil.get(key);
     }
-
 }
